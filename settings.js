3ce1f895c15f49e51ec12f38fa801da12050f4f7
jospania <joshuaayoola876@gmail.com>
2025-07-25 19:43:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'cold',
-  author: '‎',
-  botName: "Cold's bot",
-  botOwner: 'Cold', // Your name
+  author: '‎JOSPANIA😈',
+  botName: "Jospania",
+  botOwner: 'Cold...!!!😒', // Your name
   ownerNumber: '2348119732211', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Wake up to reality.....this bot is only for him. He who controls me is him",
   version: "2.0.6",
 };
 
